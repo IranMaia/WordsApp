da6421a242446063275797a7f71cd90f7a806396
Iran Maia <100581589+IranMaia@users.noreply.github.com>
2023-09-13 11:51:16 +0100

    Finalized letterListFragment
    The letterListFragment has been finalized whit all methods and functions.

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -1,31 +1,42 @@
 package com.example.wordsapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.FragmentLetterListBinding
 
-//(declarando que o valor inicial de _bindiing é nullo)
+// Variável para armazenar o binding do fragmento
+//declarando que o valor inicial de _bindiing é nullo
 private var _binding: FragmentLetterListBinding? = null
-//(captura o valor de binding. Como o valor do binding é uma view, !! garante que nao pode ser nulo.
-private var binding get() = _binding!!
+
+// Propriedade para acessar o binding (não pode ser nula)
+//Como o valor do binding é uma view, !! garante que nao pode ser nulo.
+private val binding get() = _binding!!
 private lateinit var recyclerView: RecyclerView
+private var isLinearLayoutManager = true
 
 class LetterListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true) //Informa que o sitema possui um menu.
+        //Informa que o sitema possui um menu.
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
+    // Infla o layout do fragmento usando o binding
     ): View? {
         _binding = FragmentLetterListBinding.inflate(inflater, container, false)
         val view = binding.root
@@ -33,36 +44,65 @@ class LetterListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Inicializa a RecyclerView e escolhe o layout
         recyclerView = binding.recyclerView
         chooseLayout()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        // Libera a referência ao binding no momento da destruição da view
         _binding = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // Infla o menu de layout
         inflater.inflate(R.menu.layout_menu, menu)
 
+        // Configura o ícone do botão de layout com base na orientação atual
         val layoutButton = menu.findItem(R.id.action_switch_layout)
         setIcon(layoutButton)
     }
 
+    // Função para escolher o layout da RecyclerView
+    private fun chooseLayout() {
+        when (isLinearLayoutManager) {
+            true -> {
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = LetterAdapter()
+            }
+
+            false -> {
+                recyclerView.layoutManager = GridLayoutManager(context, 4)
+                recyclerView.adapter = LetterAdapter()
+            }
+        }
+    }
+
+    // Função para configurar o ícone do botão de layout
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                //Sets isLinearLayoutManager (a Boolean) to the opposite value
+                isLinearLayoutManager = !isLinearLayoutManager
+                // Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
